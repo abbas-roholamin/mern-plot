6e120c59ba284b80c982ad88d2532278cf99c3ec
Abbas Roholamin <abbasroholamin76@gmail.com>
2023-02-14 17:53:25 +0430

    Create userController.js

---
+++ natours/controllers/userController.js
@@ -0,0 +1,42 @@
+const index = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
+const create = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
+const show = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
+const update = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
+const destory = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
+module.exports = {
+  index,
+  show,
+  create,
+  update,
+  destory,
+};
