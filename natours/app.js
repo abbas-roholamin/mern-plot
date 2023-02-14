fcdda4abef15f985c8186c141f9234bb80092233
Abbas Roholamin <abbasroholamin76@gmail.com>
2023-02-14 17:54:56 +0430

    Update app.js
    Extract routes and controllers

--- natours/app.js
+++ natours/app.js
@@ -1,79 +1,14 @@
-const { json } = require('body-parser');
 const express = require('express');
-const fs = require('fs');
 const morgan = require('morgan');
 const app = express();
+const router = require('./routes/web');
 
 // Middleware
 app.use(express.json());
 app.use(morgan('dev'));
 
-const dataPath = `${__dirname}/dev-data/data/tours-simple.json`;
-const tours = JSON.parse(fs.readFileSync(dataPath));
-
-// Resolvers
-const getALlTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    length: tours.length,
-    data: {
-      tours,
-    },
-  });
-};
-
-const createTour = (req, res) => {
-  const id = tours[tours.length - 1].id + 1;
-  const newTour = { id, ...req.body };
-  tours.push(newTour);
-  fs.writeFile(dataPath, JSON.stringify(tours, null, 2), (err) => {
-    res
-      .status(201)
-      .json({ status: 'success', length: tours.length, data: newTour });
-  });
-};
-
-const getTour = (req, res) => {
-  const { id } = req.params;
-  const tour = tours.find((t) => t.id === Number(id));
-
-  if (!tour) {
-    res.status(404).json({ status: 'NOT FOUND' });
-  }
-
-  res.status(200).json({ status: 'success', data: { tour } });
-};
-
-const updateTour = (req, res) => {
-  if (Number(req.params.id) > tours.length) {
-    res.status(404).json({ status: 'Invalid ID' });
-  }
-
-  res.status(200).json({ status: 'success', data: { tour: 'PATCH' } });
-};
-
-const deleteTour = (req, res) => {
-  if (Number(req.params.id) > tours.length) {
-    res.status(404).json({ status: 'Invalid ID' });
-  }
-
-  res.status(204).json({ status: 'success', data: null });
-};
-
-// Get All Tours
-app.get('/api/v1/tours', getALlTours);
-
-// Add New Tour
-app.post('/api/v1/tours', createTour);
-
-// Get Tour
-app.get('/api/v1/tours/:id', getTour);
-
-// Update Tour
-app.patch('/api/v1/tours/:id', updateTour);
-
-// Delete Tour
-app.delete('/api/v1/tours/:id', deleteTour);
+// Router
+router(app);
 
 // Listening on port
 const PORT = process.env.PORT || 5000;
