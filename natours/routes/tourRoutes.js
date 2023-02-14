82212af6a5c7449d5a2daf4419fcd30ffd00f7db
Abbas Roholamin <abbasroholamin76@gmail.com>
2023-02-14 18:22:08 +0430

    Update tourRoutes.js
    Add hasValidBody middleware

--- natours/routes/tourRoutes.js
+++ natours/routes/tourRoutes.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const tourController = require('./../controllers/tourController');
+const { hasValidBody } = require('../middlewares/checkValidBody');
 
 router.param('id', tourController.isValidId);
 
 router.get('/', tourController.index);
-router.post('/', tourController.create);
+router.post('/', hasValidBody, tourController.create);
 router.get('/:id', tourController.show);
 router.patch('/:id', tourController.update);
 router.delete('/:id', tourController.destory);
