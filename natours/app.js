8997f6f7628eb9127064d54478688c87143f4c69
Abbas Roholamin <abbasroholamin76@gmail.com>
2023-02-14 18:35:58 +0430

    Update app.js
    Loading environment variables

--- natours/app.js
+++ natours/app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const morgan = require('morgan');
+const dotenv = require('dotenv');
 const app = express();
 const router = require('./routes/web');
 
-// Middleware
+// Load environment variables
+dotenv.config({ path: './.env' });
+
+// Middlewares
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(express.static(`${__dirname}/public`));
 
 // Router
 router(app);
