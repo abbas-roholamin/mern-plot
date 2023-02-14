f665b0b12cd1489dd169c0dcb7bbeb05e5c644ac
Abbas Roholamin <abbasroholamin76@gmail.com>
2023-02-14 18:04:10 +0430

    Update tourRoutes.js
    Add Param middleware

--- natours/routes/tourRoutes.js
+++ natours/routes/tourRoutes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const tourController = require('./../controllers/tourController');
 
+router.param('id', tourController.isValidId);
+
 router.get('/', tourController.index);
 router.post('/', tourController.create);
 router.get('/:id', tourController.show);
