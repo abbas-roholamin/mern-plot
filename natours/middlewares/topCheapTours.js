cae89595c5a469a6689e60947a05bd840a8f89a3
Abbas Roholamin <abbasroholamin76@gmail.com>
2023-02-25 21:12:07 +0430

    Create topCheapTours.js

---
+++ natours/middlewares/topCheapTours.js
@@ -0,0 +1,8 @@
+const topCheapTours = function (req, res, next) {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
+
+module.exports = topCheapTours;
