0064eb15377982f0e2c79c88dc2615b006c2a68f
Abbas Roholamin <abbasroholamin76@gmail.com>
2023-03-01 20:29:04 +0430

    Update web.js
    Handle unhandled routes

--- natours/routes/web.js
+++ natours/routes/web.js
@@ -4,4 +4,9 @@ const userRouter = require('./userRoutes');
 module.exports = (app) => {
   app.use('/api/v1/tours', tourRouter);
   app.use('/api/v1/users', userRouter);
+  app.all('*', (req, res, next) => {
+    res
+      .status(404)
+      .json({ status: 'fail', message: `Can't found ${req.originalUrl}` });
+  });
 };
