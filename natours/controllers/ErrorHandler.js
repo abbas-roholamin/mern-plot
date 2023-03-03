4fa4e1081ce447bdaa00b696189b374778ab11f7
Abbas Roholamin <abbasroholamin76@gmail.com>
2023-03-03 17:53:18 +0430

    Update ErrorHandler.js
    Handling development and production errors

--- natours/controllers/ErrorHandler.js
+++ natours/controllers/ErrorHandler.js
@@ -1,11 +1,36 @@
+const devError = (res, err) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const prodError = (res, err) => {
+  if (err.isOpertional) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.log('Error', err);
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong!',
+    });
+  }
+};
+
 const ErrorHandler = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV !== 'production') {
+    prodError(res, err);
+  } else if (process.env.NODE_ENV === 'development') {
+    devError(res, err);
+  }
 };
 
 module.exports = ErrorHandler;
