2a2e8d8e5570f30314dd16ad403c0190a8d06c58
Abbas Roholamin <abbasroholamin76@gmail.com>
2023-03-04 22:48:48 +0430

    Update ErrorHandler.js
    Handling defriend kind of database site error

--- natours/controllers/ErrorHandler.js
+++ natours/controllers/ErrorHandler.js
@@ -1,3 +1,25 @@
+const Abort = require('./../utils/Abort');
+
+const handleCastError = err => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new Abort(message, 400);
+};
+
+const handleDuplicateFields = err => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  console.log(value);
+
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new Abort(message, 400);
+};
+
+const handleValidationError = err => {
+  const errors = Object.values(err.errors).map(el => el.message);
+
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new Abort(message, 400);
+};
+
 const devError = (res, err) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -27,7 +49,14 @@ const ErrorHandler = (err, req, res, next) => {
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV !== 'production') {
-    prodError(res, err);
+    let error = { ...err };
+
+    if (error.name === 'CastError') error = handleCastError(error);
+    if (error.code === 11000) error = handleDuplicateFields(error);
+    if (error.name === 'ValidationError') error = handleValidationError(error);
+
+
+    prodError(res, error);
   } else if (process.env.NODE_ENV === 'development') {
     devError(res, err);
   }
